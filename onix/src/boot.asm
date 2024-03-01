7fad5b58bc5eae6aa6863ab6a4a344e3ae620e6d
kksty <2731502523@qq.com>
2024-03-01 22:26:10 +0800

    2024.3.3 23:16

--- onix/src/boot.asm
+++ onix/src/boot.asm
@@ -16,9 +16,45 @@ xchg bx,bx ;bochs魔术断点
 mov si, booting
 call printf
 
+mov edi, 0x1000 ;读取的目标内存
+mov ecx, 0 ;起始扇区
+mov bl, 1 ;读取的扇区数量
+call read_disk
+
 ; 阻塞
 jmp $
 
+read_disk:
+    ; 设置读写扇区的数量
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
+
+    inc dx ;移动到0x1f3
+    mov al, cl; 起始扇区的前八位
+    out dx, al
+
+    inc dx ;移动到0x1f4
+    shr ecx, 8
+    mov al, cl; 起始扇区的中八位
+    out dx, al
+
+    inc dx ;移动到0x1f5
+    shr ecx, 8
+    mov al, cl; 起始扇区的高八位
+    out dx, al
+
+    inc dx;0x1f6
+    shr ecx, 8 
+    and cl, 0b1111 ;将高四位置为0
+    
+    mov al, 0b1110_0000 
+    or al, cl
+    out dx, al
+
+    
+
+    ret
 printf:
     mov ah,0x0e ; BIOS中断0x10用于打印字符的功能码。
 .next:
